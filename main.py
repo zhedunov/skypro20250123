09756caa37718f0a9efccad035086421a45bab0e
Ruslan Zhedunov <jake_r7@mail.ru>
2025-01-26 12:08:01 +0400

    Added fake widget file with fake 2 functions

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from src.masks import *
+from src.widget import *
 
-card_number = "7000792289606361"
-# ~ card_number = "70007922896063"
-card_number_mask = get_mask_card_number(card_number)
-print(card_number_mask)  # 7000 79** **** 6361
+# ~ card_number = "7000792289606361"
+card_number = "70007922896063"
+# ~ card_number_mask = get_mask_card_number(card_number)
+# ~ print(card_number_mask)  # 7000 79** **** 6361
 
-account_number = "73654108430135874305"
-# ~ account_number = "7365410843013587430"
-account_number_mask = get_mask_account(account_number)
-print(account_number_mask)  # **4305
+# ~ account_number = "73654108430135874305"
+account_number = "7365410843013587430"
+# ~ account_number_mask = get_mask_account(account_number)
+# ~ print(account_number_mask)  # **4305
+
+card_number = "Maestro 1596837868705199"
+card_number = "Счет 64686473678894779589"
+card_number = "MasterCard 7158300734726758"
+card_number = "Счет 35383033474447895560"
+card_number = "Visa Classic 6831982476737658"
+card_number = "Visa Platinum 8990922113665229"
+card_number = "Visa Gold 5999414228426353"
+card_number = "Счет 73654108430135874305"
+
+s = mask_account_card(card_number)
+print(s)  # **4305
+
+date_given = "2024-03-11T02:26:18.671407"
+s = get_date(date_given)
+print(s)  # **4305
+
---
+++ src/widget.py
@@ -0,0 +1,20 @@
+def mask_account_card(card_number: str) -> str:
+    """Функция маскирует номер карты"""
+    return "imitate mask_account_card"
+
+
+def get_date(date_string: str) -> str:
+    """Функция меняет формат даты на российский"""
+    return "imitate get_date"
+
+
+# ~ card_number = "7000792289606361"
+# ~ card_number = "70007922896063"
+
+# ~ card_number_mask = get_mask_card_number(card_number)
+# ~ print(card_number_mask)  # 7000 79** **** 6361
+
+# ~ account_number = "73654108430135874305"
+# ~ account_number = "7365410843013587430"
+# ~ account_number_mask = get_mask_account(account_number)
+# ~ print(account_number_mask)  # **4305
