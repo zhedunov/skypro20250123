6f6c017987c05ae7eff23004ef20f8dc8dc10fbf
Ruslan Zhedunov <jake_r7@mail.ru>
2025-01-26 19:57:24 +0400

    Functions mask_account_card and get_date were realised

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 from src.masks import *
 from src.widget import *
 
-# ~ card_number = "7000792289606361"
-card_number = "70007922896063"
-# ~ card_number_mask = get_mask_card_number(card_number)
-# ~ print(card_number_mask)  # 7000 79** **** 6361
-
-# ~ account_number = "73654108430135874305"
-account_number = "7365410843013587430"
-# ~ account_number_mask = get_mask_account(account_number)
-# ~ print(account_number_mask)  # **4305
 
 card_number = "Maestro 1596837868705199"
 card_number = "Счет 64686473678894779589"
@@ -20,10 +11,12 @@ card_number = "Visa Platinum 8990922113665229"
 card_number = "Visa Gold 5999414228426353"
 card_number = "Счет 73654108430135874305"
 
-s = mask_account_card(card_number)
-print(s)  # **4305
+# s = mask_account_card(card_number)
+# print(s)  # **4305
 
 date_given = "2024-03-11T02:26:18.671407"
 s = get_date(date_given)
 print(s)  # **4305
+
+
 
