7dece80269e50a00e2f3819d21497e3c34e6700a
Ruslan Zhedunov <jake_r7@mail.ru>
2025-02-07 12:54:45 +0400

    functions checked with linters, etc

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import src.masks
 import src.widget
 import src.processing
 
-'''
+"""
 card_number = "Maestro 1596837868705199"
 card_number = "Счет 64686473678894779589"
 card_number = "MasterCard 7158300734726758"
@@ -18,16 +18,25 @@ card_number = "Счет 73654108430135874305"
 date_given = "2024-03-11T02:26:18.671407"
 s = src.widget.get_date(date_given)
 print(s)  # **4305
-'''
+"""
 
 
-test_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-#res = src.processing.filter_by_state(test_data, 'EXECUTED')
-#print(res)
+test_data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+res = src.processing.filter_by_state(test_data, "EXECUTED")
+print(res)
 
-test_data2 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+test_data2 = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 res = src.processing.sort_by_date(test_data2)
 print(res)
 res = src.processing.sort_by_date(test_data2, True)
 print(res)
-
--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,17 @@
-def filter_by_state(operations, status="EXECUTED"):
+from xmlrpc.client import boolean
+
+
+def filter_by_state(operations: list, status: str = "EXECUTED") -> list:
+    """Функция выбирает операции с указанным статусом"""
     res = []
     for z in operations:
-        if z.get('state') == status:
+        if z.get("state") == status:
             res.append(z)
     return res
 
 
-def sort_by_date(operations, decrease=True):
-
-    res = sorted(operations, key=lambda d: d.get('date'), reverse=decrease)
+def sort_by_date(operations: list, decrease: bool = True) -> list:
+    """Функция сортирует операции по их дате"""
+    res = sorted(operations, key=lambda d: d.get("date"), reverse=decrease)
 
     return res
