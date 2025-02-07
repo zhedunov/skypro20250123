524b6288b2529e0d58b18ed04064db32b9c49cb5
Ruslan Zhedunov <jake_r7@mail.ru>
2025-02-07 12:16:41 +0400

    filter_by_state function realised

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ print(s)  # **4305
 
 
 test_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-res = src.processing.filter_by_state(test_data, 'CANCELED')
+res = src.processing.filter_by_state(test_data, 'EXECUTED')
 print(res)
 '''
 test_data2 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
